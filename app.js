0da131f2d8580d610274b27b395af732b7e65cdf
LuciaYuhasz <luciayuhasz@gmail.com>
2025-04-18 10:52:01 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//app.js
+const express = require('express');
+const path = require('path');
+const gameRoutes = require('./gameRoutes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Rutas
+app.use('/api', gameRoutes);
+// Ruta principal
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+
+// Iniciar servidor
+app.listen(PORT, () => console.log(`Servidor ejecutándose en http://localhost:${PORT}`));
